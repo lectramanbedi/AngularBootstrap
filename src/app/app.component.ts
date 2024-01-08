3bf3eb1dab600d2e6beb43ce82e910bb8ecf5c85
lect_raman_bedi <lect_raman_bedi@hotmail.com>
2024-01-08 08:04:05 -0600

    changes pushed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'AngularBootstrap';
+export class AppComponent implements OnInit {
+
+  registerForm: FormGroup = this.fb.group({
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    number: ['', [Validators.required, Validators.minLength(10)]],
+
+  });
+
+
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+
+  }
+  get firstName(): any {
+    return this.registerForm.get('firstName');
+  }
+  get lastName(): any {
+    return this.registerForm.get('lastName');
+  }
+  get email(): any {
+    return this.registerForm.get('email');
+  }
+  get number(): any {
+    return this.registerForm.get('number');
+  }
+  registerFormSubmit(): void {
+    const formData = this.registerForm.value;
+    console.log(formData);
+    // Api Request Here
+  }
+
 }
